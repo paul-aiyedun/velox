840c32de6c3f6f5184035489cb69deb12c0b681b
Daniel Bauer <dnb@zurich.ibm.com>
2025-11-20 08:24:13 +0100

    Update ToCudf.cpp for name change of CombinedCudfHttpExchange -> HybridExchange.

--- velox/experimental/cudf/exec/ToCudf.cpp
+++ velox/experimental/cudf/exec/ToCudf.cpp
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-#include "velox/experimental/cudf-exchange/CombinedCudfHttpExchange.h"
 #include "velox/experimental/cudf-exchange/CudfExchange.h"
 #include "velox/experimental/cudf-exchange/CudfExchangeClient.h"
 #include "velox/experimental/cudf-exchange/CudfPartitionedOutput.h"
 #include "velox/experimental/cudf-exchange/ExchangeClientFacade.h"
+#include "velox/experimental/cudf-exchange/HybridExchange.h"
 #include "velox/experimental/cudf/CudfConfig.h"
 #include "velox/experimental/cudf/connectors/hive/CudfHiveConnector.h"
 #include "velox/experimental/cudf/connectors/hive/CudfHiveDataSource.h"
@@ -341,6 +341,10 @@ bool CompileState::compile(bool allowCpuFallback) {
           getPlanNode(oper->planNodeId()));
       VELOX_CHECK(planNode != nullptr);
       keepOperator = 1;
+      // We don't make a new type of table scan operator but use the existing
+      // type. But we need to update the connector so we'll make a new plan node
+      // from the old one and use that to construct the new operator of the same
+      // type but with the updated connector
     } else if (isJoinSupported(oper)) {
       if (auto joinBuildOp = dynamic_cast<exec::HashBuild*>(oper)) {
         auto planNode = std::dynamic_pointer_cast<const core::HashJoinNode>(
@@ -428,8 +432,9 @@ bool CompileState::compile(bool allowCpuFallback) {
           (planNode->isRootFragment())) {
         keepOperator = 1;
       } else {
-        replaceOp.push_back(std::make_unique<CudfPartitionedOutput>(
-            id, ctx, planNode, partitionOp->getEagerFlush()));
+        replaceOp.push_back(
+            std::make_unique<CudfPartitionedOutput>(
+                id, ctx, planNode, partitionOp->getEagerFlush()));
       }
     } else if (auto exchangeOp = dynamic_cast<exec::Exchange*>(oper)) {
       auto planNode = std::dynamic_pointer_cast<const core::ExchangeNode>(
@@ -447,7 +452,7 @@ bool CompileState::compile(bool allowCpuFallback) {
           // The following std::move transfers the ownership of the
           // HttpExchangeClient to the facade, preventing that it is closed when
           // the ExchangeOperator is destructed after being replace by the
-          // CombinedCudfHttpExchange.
+          // HybridExchange.
           auto veloxExchangeClient = exchangeOp->releaseExchangeClient();
           VELOX_CHECK_NOT_NULL(
               veloxExchangeClient, "Velox exchange client can't be null.");
@@ -455,8 +460,7 @@ bool CompileState::compile(bool allowCpuFallback) {
           auto cudfClient = std::make_shared<CudfExchangeClient>(
               oper->taskId(),
               veloxExchangeClient->getDestination(),
-              veloxExchangeClient->getNumberOfConsumers(),
-              veloxExchangeClient->getExecutor());
+              veloxExchangeClient->getNumberOfConsumers());
           client = std::make_shared<ExchangeClientFacade>(
               std::move(cudfClient), std::move(veloxExchangeClient));
           TaskPlanNodeKey key(oper->taskId(), oper->planNodeId());
@@ -467,8 +471,8 @@ bool CompileState::compile(bool allowCpuFallback) {
           // destructed after being replaced by the ExchangeClientFacade
           exchangeOp->resetExchangeClient();
         }
-        replaceOp.push_back(std::make_unique<CombinedCudfHttpExchange>(
-            id, ctx, planNode, client));
+        replaceOp.push_back(
+            std::make_unique<HybridExchange>(id, ctx, planNode, client));
       }
     } else if (
         auto localExchangeOp = dynamic_cast<exec::LocalExchange*>(oper)) {
@@ -482,11 +486,11 @@ bool CompileState::compile(bool allowCpuFallback) {
             std::dynamic_pointer_cast<const core::MergeExchangeNode>(
                 getPlanNode(oper->planNodeId()));
         VELOX_CHECK(planNode != nullptr);
-        // create a CombinedCudfHttpExchange operator for the merge exchange.
-        // Pass a nullptr to force the CombinedCudfHttpExchange op to create its
+        // create a HybridExchange operator for the merge exchange.
+        // Pass a nullptr to force the HybridExchange op to create its
         // own, private CudfExchangeClient.
-        replaceOp.push_back(std::make_unique<CombinedCudfHttpExchange>(
-            id, ctx, planNode, nullptr));
+        replaceOp.push_back(
+            std::make_unique<HybridExchange>(id, ctx, planNode, nullptr));
         // Add an order-by node. SortingKeys and SortOrders will be taken from
         // the MergeExchangeNode.
         replaceOp.push_back(std::make_unique<CudfOrderBy>(id, ctx, planNode));
@@ -517,7 +521,6 @@ bool CompileState::compile(bool allowCpuFallback) {
           exec::StreamingAggregation,
           exec::Limit,
           exec::LocalPartition,
-          exec::LocalExchange,
           exec::FilterProject,
           exec::AssignUniqueId,
           CudfOperator>(op);
@@ -541,10 +544,14 @@ bool CompileState::compile(bool allowCpuFallback) {
       LOG(INFO) << "GPU operator condition = " << condition << std::endl;
     }
     if (!allowCpuFallback) {
-      VELOX_CHECK(condition, "Replacement with cuDF operator failed");
+      VELOX_CHECK(
+          condition,
+          "Replacement with cuDF operator failed. Falling back to CPU execution for operator: {}",
+          oper->toString());
     } else if (!condition) {
       LOG(WARNING)
-          << "Replacement with cuDF operator failed. Falling back to CPU execution";
+          << "Replacement with cuDF operator failed. Falling back to CPU execution for operator:"
+          << oper->toString();
     }
 
     if (not replaceOp.empty()) {
